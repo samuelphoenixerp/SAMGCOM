6bc9dad5e99ade052d86ce75ff66e2877d5d79a8
samuelphoenixerp <94829027+samuelphoenixerp@users.noreply.github.com>
2021-11-23 12:11:36 +0200

    ADDED WEB FILES
    ADDED WEB FILES

---
+++ WebAppTest/WebAppTest/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!this.IsPostBack)
+            {
+
+
+                Response.Write("My New Clone SamWork2 LOCAL from 146!!");
+               
+            }
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+
+            Response.Write("Samuel");
+        }
+    }
+}
